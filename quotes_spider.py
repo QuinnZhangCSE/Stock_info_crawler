3799a76aabbbb31fbac06ac4ccb9f5b30ff70b90
QuinnZhangCSE <47461593+QuinnZhangCSE@users.noreply.github.com>
2019-09-03 22:59:42 -0400

    Add files via upload

---
+++ quotes_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    
+    def start_requests(self):
+        target = "FB"
+        url = "https://finance.yahoo.com/quote/" + target + "/history"
+        yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        chart = []
+        for info in response.xpath('//span/text()').getall():
+            chart.append(info)
+        #chart = chart[28:238]
+        day_info = []
+        for i in range(21,238,7):
+            day_info.append(chart[i:i+7])
+        for info in day_info:
+            print(info)
